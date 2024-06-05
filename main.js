0a4894844cb95627178ff695df4cc691009defda
Zor Abbanock <emirate1909@gmail.com>
2024-06-05 22:04:34 +0400

    generator02

---
+++ main.js
@@ -0,0 +1,13 @@
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklomnopqrstuvwxyz";
+const numbers = "1234567890"
+const symbols = "!-$^+";
+const space = " "
+
+const spacesCheckbox = document.querySelector("spaces")
+const notrepeatsCheckbox = document.querySelector("none-repeats")
+const uppersCheckbox = document.querySelector("uppers")
+const lowersCheckbox = document.querySelector("lowers")
+const specialsCheckbox = document.querySelector("specials")
+const numbersCheckbox = document.querySelector("numbers")
+const spacesCheckbox = document.querySelector("spaces")
